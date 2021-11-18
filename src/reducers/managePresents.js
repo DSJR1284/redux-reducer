c04d5c59b24e358140e61416313adc124795e5d6
Doug Jones <DSJR1284@GMAIL.COM>
2021-11-17 22:23:58 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,15 @@
-export function managePresents(state, action){
-}
+//https://redux.js.org/tutorials/fundamentals/part-3-state-actions-reducers
+
+export function managePresents(state = {
+    numberOfPresents: 0,
+}, action) {
+    switch(action.type) {  
+    case "INCREASE":
+        return Object.assign({}, state, {
+        numberOfPresents: state.numberOfPresents + 1
+        });  
+    default:
+        return state;
+    }
+};
+
